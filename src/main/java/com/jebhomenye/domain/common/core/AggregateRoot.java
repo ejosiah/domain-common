36939ccd2ace5ad8ea22e94a4113cded761c6e4f
joebhomenye@gmail.com <jay@Jay-workstation>
2013-08-28 17:30:38 +0100

    made implementation of the momento contract optional

--- src/main/java/com/jebhomenye/domain/common/core/AggregateRoot.java
+++ src/main/java/com/jebhomenye/domain/common/core/AggregateRoot.java
@@ -13,13 +13,20 @@ import lombok.SneakyThrows;
 import com.jebhomenye.domain.common.event.DomainEvent;
 import com.jebhomenye.domain.common.event.DomainEventPublisher;
 
+/**
+ * A collection of objects that are bound together by a root entity, otherwise known as an aggregate root. 
+ * The aggregate root guarantees the consistency of changes being made within the aggregate by forbidding 
+ * external objects from holding references to its members
+ * @author jay
+ *
+ * @param <E> Type of root entity
+ * @param <T> Type of root entity's identity
+ * @param <M> Type of Momento object for this aggregate root
+ */
 public abstract class AggregateRoot<E, T extends Identity<? extends Serializable>, M extends Momento> 
 		implements ConcurrentModificationSafeEntity<E, T>, Originator<M> {
-	
-	private transient LinkedList<DomainEvent> changes = new LinkedList<>();
-	
+		
 	public <D extends DomainEvent>void apply(D event){
-		changes.add(event);
 		mutate(event);
 		DomainEventPublisher
 		  .instance()
@@ -50,6 +57,16 @@ public abstract class AggregateRoot<E, T extends Identity<? extends Serializable
 		for(DomainEvent event : events){
 			mutate(event);
 		}
+	}
+
+	@Override
+	public M takeSnapShot() {
+		throw new UnsupportedOperationException("should be implemented by subclass that requre");
+	}
+
+	@Override
+	public void restoreFrom(M momento) {
+		throw new UnsupportedOperationException("should be implemented by subclass that requre");
 	}
 
 }
